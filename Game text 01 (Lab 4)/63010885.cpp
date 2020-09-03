45a9a19f869f3c8dadd59f3697cc9f8d5e515c6f
wittawas19@gmail.com <wittawas19@gmail.com>
2020-09-03 15:44:39 +0700

    New algorithm

--- Game text 01 (Lab 4)/63010885.cpp
+++ Game text 01 (Lab 4)/63010885.cpp
@@ -6,75 +6,51 @@
 void gotoxy(int x, int y);
 void draw_ship(int x, int y);
 void erase_ship(int x, int y);
-// ลอลคิดกลับดูเป็นจาก condition = printf เป็น
 int main()
 {
 	char ch = ' ';
-	int x = 79 , y = 0;
+	int x = 40 , y = 5;
 	draw_ship(x, y);
 	do {
 		if (_kbhit()) {
 			ch = _getch();
-			if (x < 81 && y > 0 && x > 0 && y < 24 )
+			if (ch == 'a') //ลองเปลี่ยนจาก ใส่เงื่อนไขถึงกดได้ เป็นถ้ากด ให้ทำตามเงื่อนไขแทน
 			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'd') {draw_ship(++x, y) ; }
-				if (ch == 'w') {draw_ship(x, --y);erase_ship(x, y+1); }
-				if (ch == 's') {draw_ship(x, ++y);erase_ship(x, y-1); }
-				fflush(stdin);
+				if (x > 0)
+				{
+					draw_ship(x--, y);
+				}
+				else
+				{
+					draw_ship(x, y); 
+				}
 			}
-			else if (y <= 0 && x >= 81)
+			if (ch == 'd')
 			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 's') { draw_ship(x, ++y); erase_ship(x, y - 1); }
-				fflush(stdin);
+				if (x < 81)
+				{
+					draw_ship(x++, y);
+				}
+				else
+				{
+					draw_ship(x, y);
+				}
 			}
-			else if (x <= 0 && y <= 0)
+			if (ch == 'w')
 			{
-				if (ch == 'd') { draw_ship(++x, y); }
-				if (ch == 's') { draw_ship(x, ++y); erase_ship(x, y - 1); }
-				fflush(stdin);
+				if(y > 0)
+				{
+					draw_ship(x, --y);
+					erase_ship(x, y + 1);
+				}
 			}
-			else if (x <= 0 && y >= 24)
+			if (ch == 's')
 			{
-				if (ch == 'd') { draw_ship(++x, y); }
-				if (ch == 'w') { draw_ship(x, --y); erase_ship(x, y + 1); }
-				fflush(stdin);
-			}
-			else if (y >= 24 && x >= 81)
-			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'w') { draw_ship(x, --y); erase_ship(x, y + 1); }
-				fflush(stdin);
-			}
-			else if (x <= 0)
-			{
-				if (ch == 'd') { draw_ship(++x, y); }
-				if (ch == 'w') { draw_ship(x, --y); erase_ship(x, y + 1); }
-				if (ch == 's') { draw_ship(x, ++y); erase_ship(x, y - 1); }
-				fflush(stdin);
-			}
-			else if (y >= 24)
-			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'd') { draw_ship(++x, y); }
-				if (ch == 'w') { draw_ship(x, --y); erase_ship(x, y + 1); }
-				fflush(stdin);
-			}
-			else if (x >= 81)
-			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'w') { draw_ship(x, --y); erase_ship(x, y + 1); }
-				if (ch == 's') { draw_ship(x, ++y); erase_ship(x, y - 1); }
-				fflush(stdin);
-			}
-			else if (y <= 0)
-			{
-				if (ch == 'a') { draw_ship(--x, y); }
-				if (ch == 'd') { draw_ship(++x, y); }
-				if (ch == 'w') { draw_ship(x, y); erase_ship(x, y + 1); }
-				if (ch == 's') { draw_ship(x, ++y); erase_ship(x, y - 1); }
-				fflush(stdin);
+				if (y < 24)
+				{
+					draw_ship(x, ++y);
+					erase_ship(x, y - 1);
+				}
 			}
 		
 		}
@@ -82,7 +58,6 @@ int main()
 	} while (ch != 'x');
 	return 0;
 }
-
 
 
 
@@ -101,5 +76,4 @@ void gotoxy(int x, int y)
 {
 	COORD c = { x , y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
-	
 }
